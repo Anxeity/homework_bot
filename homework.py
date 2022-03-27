0ed8e425765ee15b0a017926e526013d036ddea2
Михаил Глазов <glazov_52@mail.ru>
2022-03-27 14:52:45 +0300

    FirstReview

--- homework.py
+++ homework.py
@@ -56,7 +56,7 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     """Функция отправляет запрос к апи."""
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp }
+    params = {'from_date': timestamp}
     try:
         homework_statuses = requests.get(
             ENDPOINT,
